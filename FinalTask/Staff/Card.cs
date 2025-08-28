4f9933533f5ab50fb5f15404154a29578a7ccbc2
cldz <ggeennbbyy@gmail.com>
2025-08-28 12:34:05 +0300

    Add card

---
+++ FinalTask/Staff/Card.cs
@@ -0,0 +1,16 @@
+﻿using FinalTask.Utils;
+
+namespace FinalTask.Staff
+{
+    public struct Card
+    {
+        public readonly CardType Type;
+        public readonly CardValue Value;
+
+        public Card(CardType type, CardValue value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+}

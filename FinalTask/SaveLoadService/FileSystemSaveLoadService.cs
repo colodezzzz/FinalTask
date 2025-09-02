0564af810fc2556f0f42de5da7e5b7065ac83408
cldz <ggeennbbyy@gmail.com>
2025-09-02 14:15:40 +0300

    Return saveload service

---
+++ FinalTask/SaveLoadService/FileSystemSaveLoadService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask.SaveLoadService
+{
+    class FileSystemSaveLoadService : ISaveLoadService<string>
+    {
+        private const string DEFAULT_FILES_EXTENSION = ".txt";
+
+        private string _dataPath;
+
+        public FileSystemSaveLoadService(string path)
+        {
+            _dataPath = path;
+        }
+
+        public string LoadData(string id)
+        {
+            string path = Path.Combine(_dataPath, GetFileName(id));
+
+            if (File.Exists(path))
+            {
+                using (StreamReader readStream = File.OpenText(path))
+                {
+                    string result = readStream.ReadToEnd();
+                    return result;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Can't load data! File doesn't exist!");
+            }
+
+            return default;
+        }
+
+        public void SaveData(string data, string id)
+        {
+            string path = Path.Combine(_dataPath, GetFileName(id));
+
+            if (File.Exists(path) == false)
+            {
+                File.Create(path);
+            }
+
+            using (StreamWriter writeStream = File.CreateText(path))
+            {
+                writeStream.WriteLine(data);
+            }
+        }
+
+        private string GetFileName(string fileName)
+        {
+            return fileName + DEFAULT_FILES_EXTENSION;
+        }
+    }
+}
